4c2f6c8b01a3b015dd7f8decf9fb1555ea9b6e4f
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-29 14:27:26 -0500

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,45 @@
+variable "deployment_username" {
+
+}
+
+variable "deployment_password" {
+
+}
+
+variable "deployment_client_id" {
+}
+
+variable "deployment_client_secret" {
+
+}
+
+variable "deployment_subscription_id" {
+
+}
+variable "deployment_tenant_id" {
+
+}
+variable "location" {
+  default     = "eastus"
+  description = "Azure location"
+  
+}
+
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "app" {
+  type        = string
+  description = "Name of application"
+  default     = "terraform-kata-demo-amazing"
+}
+variable "zone" {
+  default = "us-east-2b"
+}
+variable "docker-image" {
+  type        = string
+  description = "name of the docker image to deploy"
+  default     = "chrisgallivan/kata-friday:latest"
+}
